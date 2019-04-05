97b81060f429261cb28fa6118ad5c8dd7dd0bc16
thedevsjourney <thedevsjourney@gmail.com>
2019-04-05 10:07:06 -0500

    Updates

---
+++ precache-manifest.dfa173524f4f68eedae7532f9c08c509.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fc178644013cf91805ad90d2d2361cf",
+    "url": "/FoodieGoodie/static/media/bgimg.5fc17864.jpeg"
+  },
+  {
+    "revision": "032241e0b4f6d8ad35cf",
+    "url": "/FoodieGoodie/static/js/runtime~main.b56a8a4e.js"
+  },
+  {
+    "revision": "72a1578b206f17e84e74",
+    "url": "/FoodieGoodie/static/js/main.549353e8.chunk.js"
+  },
+  {
+    "revision": "587204583d1ae4903e0e",
+    "url": "/FoodieGoodie/static/js/2.efb58ad5.chunk.js"
+  },
+  {
+    "revision": "72a1578b206f17e84e74",
+    "url": "/FoodieGoodie/static/css/main.b944764a.chunk.css"
+  },
+  {
+    "revision": "e14c6a8ffb8d416869e62974ba3818a9",
+    "url": "/FoodieGoodie/index.html"
+  }
+];
